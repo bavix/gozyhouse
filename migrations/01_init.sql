e1b1ea3f394e06ef4f60185e40666cbc33f17fa7
Maxim Babichev <maksim.babichev95@gmail.com>
2023-07-01 01:14:02 +0300

    update contracts

--- migrations/01_init.sql
+++ migrations/01_init.sql
@@ -1,34 +1,36 @@
+SET allow_experimental_object_type = 1;
+
 CREATE TABLE events
 (
-    `trace_id`       UUID COMMENT 'Idempotency Key',
+    `trace_id`         UUID COMMENT 'Idempotency Key. Save from meta',
 
-    `gzy_id`         UUID COMMENT 'Client ID generated on the client side',
+    `app_id`           UUID COMMENT 'Application ID',
+    `bind_id`          UUID COMMENT 'Client ID generated on the client side',
 
-    `app_id`         UUID COMMENT 'Application ID',
-    `metadata`       String COMMENT 'Any data related to the application',
+    `meta`             Map(LowCardinality(String), String) COMMENT 'Any data related to the application',
 
-    `request_url`    String COMMENT 'Link to the page (without query data)',
-    `request_query`  String COMMENT 'Query page data',
+    `request_url`      String COMMENT 'Link to the page (without query data)',
+    `request_query`    String COMMENT 'Query page data',
 
-    `referral_url`   String COMMENT 'Link to the ref.page (without query data)',
-    `referral_query` String COMMENT 'Query ref.page data',
+    `referral_url`     String COMMENT 'Link to the ref.page (without query data)',
+    `referral_query`   String COMMENT 'Query ref.page data',
 
-    `client_id`      String COMMENT 'The unique identifier of the client.',
-    `client_ip`      FixedString(16) COMMENT 'client IP',
-    `client_data`    String COMMENT 'Any additional information about the client. For example, all IP addresses from X-forwarded-for',
+    `client_id`        Nullable(UUID) COMMENT 'The unique identifier of the client.',
+    `client_ipv4`      Nullable(IPv4) COMMENT 'client IPv4',
+    `client_ipv6`      Nullable(IPv6) COMMENT 'client IPv6',
+    `client_ipv4_list` Array(IPv4) COMMENT 'client ip json array list',
+    `client_ipv6_list` Array(IPv6) COMMENT 'client ip json array list',
+    `client_data`      Map(LowCardinality(String), String) COMMENT 'Any additional information about the client. For example, all IP addresses from X-forwarded-for',
 
-    `user_agent`     String COMMENT 'Client User Agent',
-    `cookies`        String COMMENT 'Cookies from the browser',
+    `category`         LowCardinality(String) COMMENT 'Event category',
+    `name`             String COMMENT 'Event name',
+    `label`            String COMMENT 'A small description of the event',
+    `payload`          Map(LowCardinality(String), String) COMMENT 'Additional event data',
 
-    `category`       String COMMENT 'Event category',
-    `name`           String COMMENT 'Event name',
-    `label`          String COMMENT 'A small description of the event',
-    `payload`        String COMMENT 'Additional event data',
-
-    `date`           Date COMMENT 'Event date',
-    `datetime`       DateTime('Etc/UTC') COMMENT 'Event datetime'
+    `date`             Date COMMENT 'Event date',
+    `datetime`         DateTime('Etc/UTC') COMMENT 'Event datetime'
 ) ENGINE = MergeTree
-    PARTITION BY (toYYYYMM(date), app_id)
-    PRIMARY KEY (app_id, trace_id)
-    ORDER BY (app_id, trace_id, category, name, datetime)
+      PARTITION BY (toYYYYMM(date), app_id)
+      PRIMARY KEY (app_id, trace_id)
+      ORDER BY (app_id, trace_id, category, name, datetime)
 ;
